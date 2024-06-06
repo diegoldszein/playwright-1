f37701adc5f4c088a0213bc934f19b50971377c3
Diego Goldszein <diego.goldszein@codesmithdev.com>
2024-06-06 14:04:19 -0300

    2

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -6,7 +6,7 @@ test('test', async ({ page }) => {
   const Login = new LoginPage(page)
 
   await Login.gotoLoginPage()
-  await Login.login('tomsmith@gmail.com', 'SuperSecretPassword!')
+  await Login.login('tosmith@gmail.com', 'SuperSecretPassword!')
 
   // await page.goto('https://the-internet.herokuapp.com/login');
   // await page.getByLabel('Username').click();
